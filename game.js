2ded257c18958bfbfd7a3c1b219c4da64e8d26ea
ranz-coder <ptraagames1331@gmail.com>
2026-08-19 13:26:13 +0700

    Update BACKEND_URL to new server address

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 // PENTING: Ganti string di bawah dengan URL dari Replit Anda
-const BACKEND_URL = 'https://URL_REPLIT_ANDA.repl.co';
+const BACKEND_URL = 'http://45.142.115.40:3535';
 const socket = io(BACKEND_URL); 
 
 const uiMenu = document.getElementById('ui-menu');
